a806f940030b0ab689e0789c8229f9064e84d193
simarc0s <simao.marcos.esla@gmail.com>
2024-06-06 15:53:16 +0100

    tambem usei o ruff para melhor formatacao somente

--- tablesdb.py
+++ tablesdb.py
@@ -1,27 +1,27 @@
 import sqlite3
 
 # Conectar a base de dados
-conn = sqlite3.connect('database.db')
+conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
 
 # Criar tabelas
-cursor.execute('''
+cursor.execute("""
 CREATE TABLE clientes (
     id_cliente INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
     morada TEXT NOT NULL,
     telefone TEXT NOT NULL
 );
-''')
+""")
 
-cursor.execute('''
+cursor.execute("""
 CREATE TABLE hamburgueres (
     nome_hamburguer TEXT PRIMARY KEY,
     ingredientes TEXT NOT NULL
 );
-''')
+""")
 
-cursor.execute('''
+cursor.execute("""
 CREATE TABLE pedidos (
     id_pedido INTEGER PRIMARY KEY AUTOINCREMENT,
     id_cliente INTEGER,
@@ -33,7 +33,7 @@ CREATE TABLE pedidos (
     FOREIGN KEY(id_cliente) REFERENCES clientes(id_cliente),
     FOREIGN KEY(nome_hamburguer) REFERENCES hamburgueres(nome_hamburguer)
 );
-''')
+""")
 
 # Guardar (commit) as mudanças
 conn.commit()
