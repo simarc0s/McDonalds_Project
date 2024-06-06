f21e6d58a13c4caac8e1ba4baa0bd73e7c5e8bef
simarc0s <simao.marcos.esla@gmail.com>
2024-06-06 18:04:02 +0100

    criei o "menu" de login e escrevi o codigo para conectar se ao servidor

--- main.py
+++ main.py
@@ -5,6 +5,40 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 import requests
 
+class LoginForm(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'vertical'
+
+        self.add_widget(Label(text='Username'))
+        self.username_input = TextInput()
+        self.add_widget(self.username_input)
+
+        self.add_widget(Label(text='Password'))
+        self.password_input = TextInput(password=True)
+        self.add_widget(self.password_input)
+
+        self.submit = Button(text='Login')
+        self.submit.bind(on_press=self.login)
+        self.add_widget(self.submit)
+
+    def login(self, instance):
+        data = {
+            'username': self.username_input.text,
+            'password': self.password_input.text
+        }
+        try:
+            response = requests.post('http://127.0.0.1:5000/login', json=data)
+            if response.status_code == 200:
+                token = response.json()['token']
+                self.parent.token = token
+                self.parent.show_order_form()
+            else:
+                print("Falha ao fazer login. Codigo de status:", response.status_code)
+                print("Resposta do servidor:", response.text)
+        except requests.exceptions.RequestException as e:
+            print("Erro ao conectar ao servidor:", e)
+
 class OrderForm(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -54,8 +88,9 @@ class OrderForm(BoxLayout):
             'tamanho': self.tamanho_input.text
         }
         print("Enviando dados para o servidor:", data)
+        headers = {'x-access-tokens': self.parent.token}
         try:
-            response = requests.post('http://127.0.0.1:5000/pedidos', json=data)
+            response = requests.post('http://127.0.0.1:5000/pedidos', json=data, headers=headers)
             if response.status_code == 201:
                 print("Pedido registrado com sucesso!")
             else:
@@ -64,9 +99,21 @@ class OrderForm(BoxLayout):
         except requests.exceptions.RequestException as e:
             print("Erro ao conectar ao servidor:", e)
 
+class MainApp(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.token = None
+        self.login_form = LoginForm()
+        self.order_form = OrderForm()
+        self.add_widget(self.login_form)
+
+    def show_order_form(self):
+        self.clear_widgets()
+        self.add_widget(self.order_form)
+
 class OrderApp(App):
     def build(self):
-        return OrderForm()
+        return MainApp()
 
 if __name__ == '__main__':
     OrderApp().run()
